bfa0233d17cc04d03bbef49e36c1cac9d5ddc5ba
Jean Ribeiro <contact@jeanribeiro.dev>
2023-03-10 13:08:27 -0300

    adds wallet connect

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,38 @@
 	import Counter from './Counter.svelte';
 	import welcome from '$lib/images/svelte-welcome.webp';
 	import welcome_fallback from '$lib/images/svelte-welcome.png';
+
+	import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum'
+	import { Web3Modal } from '@web3modal/html'
+	import { configureChains, createClient } from '@wagmi/core'
+	import { arbitrum, mainnet, polygon } from '@wagmi/core/chains'
+
+	const chains = [arbitrum, mainnet, polygon]
+	const projectId = '760c10756afd8450d4d96b55ef0748ec'
+
+	const { provider } = configureChains(chains, [w3mProvider({ projectId })])
+	const wagmiClient = createClient({
+		autoConnect: true,
+		connectors: w3mConnectors({ projectId, version: 1, chains }),
+		provider,
+	})
+	const ethereumClient = new EthereumClient(wagmiClient, chains)
+	const web3modal = new Web3Modal({ 
+		// @ts-ignore
+		projectId: undefined, 
+		desktopWallets: [
+			{
+				id: "Firefly",
+				name: "Firefly",
+				links: {
+					native: "firefly-alpha://connect-",
+					universal: "string",
+				},
+			},
+		],
+		enableExplorer: false,
+		}
+		, ethereumClient)
 </script>
 
 <svelte:head>
@@ -26,6 +58,8 @@
 	</h2>
 
 	<Counter />
+
+	<w3m-core-button></w3m-core-button>
 </section>
 
 <style>
